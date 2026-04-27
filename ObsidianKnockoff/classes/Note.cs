0d4bd24a24a2e7ee56004efef9d8f93a03cca1cb
Alison Doyle <S00233102@atu.ie>
2026-04-27 14:57:02 +0100

    :chore: added note class and removed notes vault

---
+++ ObsidianKnockoff/classes/Note.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObsidianKnockoff.Classes
+{
+    internal class Note
+    {
+        // properties
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        // constructor
+        public Note() { }
+
+        public Note(string title, string content)
+        {
+            Title = title; 
+            Content = content;
+        }
+
+        // methods
+        public override ToString()
+        {
+            return Title;
+        }
+    }
+}
